3ef38ea0e503b0cbe6335c3eca04bb4a81ccdd8e
Štěpán Janás <stepajanas@gmail.com>
2026-09-22 19:15:55 +0200

    Create page.tsx

---
+++ app/fyzika/page.tsx
@@ -0,0 +1,10 @@
+import FyzikaTabulky from "@/components/FyzikaTabulky";
+
+export default function FyzikaPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-8">📚 Fyzika</h1>
+      <FyzikaTabulky />
+    </div>
+  );
+}
